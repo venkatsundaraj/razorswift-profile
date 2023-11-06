591585a9d3514fb61e6400e681389b252d2f129d
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-11-06 17:56:30 +0530

    typography-changes

--- src/pages/sign-up/index.js
+++ src/pages/sign-up/index.js
@@ -1,13 +1,14 @@
 import Layout from '@/components_fbl/NavigationComponents/Layout';
-import BannerSection from '@/components_fbl/pageBasedComponents/Signup/BannerSection';
-import { Box } from '@mui/material';
+// import BannerSection from '@/src/components_fbl/pageBasedComponents/Signup/BannerSection';
+// import { Box } from '@mui/material';
 
 const page = ({}) => {
   return (
     <Layout>
-      <Box component="main">
+      {/* <Box component="main">
         <BannerSection />
-      </Box>
+      </Box> */}
+      <h1>Sign Up</h1>
     </Layout>
   );
 };
