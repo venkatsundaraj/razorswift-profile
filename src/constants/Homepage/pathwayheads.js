49d567bbb63d892b03ac07491f42894ccf9c6c87
firebrand-labs <vimalraj@firebrandlabs>
2023-12-23 18:10:24 +0530

    blogs

--- src/constants/Homepage/pathwayheads.js
+++ src/constants/Homepage/pathwayheads.js
@@ -5,6 +5,9 @@ const pathwayheads = [
     description: 'Standardise assessments and training.',
     button: 'Craft your pathway now',
     link: '/contact-us',
+    bg: '#FFD1CE',
+    lite: '#A62973',
+    dark: '#B2407D',
   },
   {
     id: 2,
@@ -12,6 +15,9 @@ const pathwayheads = [
     description: 'Be a niche specialist to achieve your potential',
     button: 'Choose your pathway now',
     link: '/signup',
+    bg: '#FFD1CE',
+    lite: '#A62973',
+    dark: '#B2407D',
   },
-]
-export default pathwayheads
+];
+export default pathwayheads;
